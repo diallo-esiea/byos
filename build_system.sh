955f740d4f2a33c2baf10a9daebc701d79a14493
DIALLO David <diallo@et.esiea.fr>
2016-04-18 18:52:48 +0200

    Utiliser fakeroot et fakechroot

--- build_system.sh
+++ build_system.sh
@@ -1,23 +1,27 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 CAT=/bin/cat
 ECHO=/bin/echo
-EXIT=exit
+MKDIR=/bin/mkdir
 MOUNT=/bin/mount
+UMOUNT=/bin/umount
 
 APT_GET=/usr/bin/apt-get
 CHROOT=/usr/sbin/chroot
-DEBOOSTRAP=/usr/sbin/deboostrap
+DEBOOTSTRAP=/usr/sbin/debootstrap
 DPKG_RECONFIGURE=/usr/sbin/dpkg-reconfigure
+FAKECHROOT=/usr/bin/fakechroot
 PASSWD=/usr/bin/passwd
 
 ARCH=amd64    		# i386, amd64
-HOSTNAME=		#
-INCLUDE=grub2,locales	#
-MIRROR=       		# 
-SUITE=squeeze 		# squeeze, wheezy, unstable, experimental
-TARGET=       		#
-VARIANT=      		# minbase, buildd, fakechroot, scratchbox
+HOSTNAME=debian
+INCLUDE=grub2,locales
+MIRROR=http://mirror.lrp/debian
+SUITE=sid           # jessie, wheezy, sid, stable, testing, unstable
+# Attention le fakeroot ne fonctionne pas avec Jessie le 18/04/2016 (cf. https://github.com/dex4er/fakechroot/pull/37)
+# => pour Jessie, remplacer FAKEROOT=<chaine vide> et FAKECHROOT=<chaine vide> et lancer build_system en root ou sudo
+TARGET=debian
+VARIANT=minbase     # minbase, buildd, fakechroot, scratchbox
 
 USAGE="$(basename "$0") [options] SUITE TARGET\n\n
 \t\tSUITE\t(lenny, squeeze, sid)\n
@@ -38,36 +42,45 @@ for i in "$@"; do
   esac
 done
 
-#${DEBOOSTRAP} --arch=${ARCH} --include=${INCLUDE} --variant=${VARIANT} ${SUITE} ${TARGET} ${MIRROR}
+${FAKECHROOT} fakeroot ${DEBOOTSTRAP} --arch=${ARCH} --include=${INCLUDE} --variant=${VARIANT} ${SUITE} ${TARGET} ${MIRROR}
 
-# Binding the virtual filesystems
-#${MOUNT} -o bind /dev ${TARGET}/dev
-#${MOUNT} -o bind /sys ${TARGET}/sys
-#${MOUNT} -t proc none ${TARGET}/proc
+# Set the hostname
+${ECHO} ${HOSTNAME} > ${TARGET}/etc/hostname
+
+${CAT} > ${TARGET}/etc/apt/sources.list << EOF
+deb ${MIRROR} ${SUITE} main contrib non-free
+deb-src ${MIRROR} ${SUITE} main contrib non-free
+
+deb ${MIRROR} ${SUITE}-updates main contrib non-free
+deb-src ${MIRROR} ${SUITE}-updates main contrib non-free
+
+deb http://security.debian.org/debian-security ${SUITE}/updates main contrib non-free
+deb-src http://security.debian.org/debian-security ${SUITE}/updates main contrib non-free
+EOF
 
 # Entering the chroot environment
-#${CHROOT} ${TARGET} /bin/bash
+${FAKECHROOT} ${CHROOT} ${TARGET} /bin/bash 
 
 # Configure locale
 #${DPKG_RECONFIGURE} locales
 
 # Create a password for root
-#{PASSWD}
-
-# Set the hostname
-#${ECHO} ${HOSTNAME} > /etc/hostname
-
-${CAT} > /etc/apt/sources.list << EOF
-deb ${MIRROR} ${SUITE} main contrib non-free
-deb-src ${MIRROR} ${SUITE} main contrib non-free
-deb ${MIRROR} ${SUITE}-updates main contrib non-free
-deb-src ${MIRROR} ${SUITE}-updates main contrib non-free
-deb http://security.debian.org/debian-security ${SUITE}/updates main contrib non-free
-deb-src http://security.debian.org/debian-security ${SUITE}/updates main contrib non-free
-EOF 
+#${PASSWD}
 
 # Update Debian package database:
 #${APT_GET} update
 
 # Quit the chroot environment
 #${EXIT}
+
+# Binding the virtual filesystems
+#${MOUNT} -o bind /dev ${TARGET}/dev
+#${MOUNT} -o bind /sys ${TARGET}/sys
+#${MOUNT} -t proc none ${TARGET}/proc
+
+# Unbinding the virtual filesystems
+#${UMOUNT} ${TARGET}/dev
+#${UMOUNT} ${TARGET}/sys
+#${UMOUNT} ${TARGET}/proc
+
+exit 0
