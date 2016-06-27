412add59c23f52f375f2c22dd88fe0e2ca4f0a77
DIALLO David <diallo@et.esiea.fr>
2016-06-27 10:42:32 +0200

    New global and system build script

--- build.sh
+++ build.sh
@@ -1,18 +1,24 @@
 #!/bin/bash -xv
 
 ECHO=/bin/echo
+MKDIR=/bin/mkdir
+MOUNT=/bin/mount
 PWD=/bin/pwd
+UMOUNT=/bin/umount
 
 BLKID=/sbin/blkid
 
-USAGE="$(basename "${0}") [options]\n
-\t\t-s=FILE, --fstab=FILE\tFSTAB output file\n"
+USAGE="$(basename "${0}") [options] DEVICE\n
+\t\tDEVICE\tRoot device name\n
+\toptions:\n
+\t--------\n
+\t\t-p=PATH, --path=PATH\tPath to install system (default=/mnt)\n"
 
 # Manage options 
 for i in "$@"; do
   case ${i} in
-    -s=*|--fstab=*)
-      FSTAB_FILE="${i#*=}"
+    -p=*|--path=*)
+      DEST_PATH="${i#*=}"
       shift
       ;;
 
@@ -24,12 +30,33 @@ for i in "$@"; do
   esac
 done
 
+if [ $# -eq 1 ]; then
+  DEVICE=${1}
+else
+  ${ECHO} -e ${USAGE}
+  exit 1
+fi
+
 # Convert relative path to absolute path
-for i in FSTAB_FILE; do 
+for i in DEST_PATH; do 
   if [[ -n "${!i}" ]] && [[ ${!i} != /* ]]; then
     eval ${i}=`${PWD}`/${!i}
   fi
 done
+
+# Assign default value in case of no option
+if [ -z "${DEST_PATH}" ]; then
+  DEST_PATH=/mnt
+else
+  # Create DEST_PATH if not exists 
+  ${MKDIR} -p ${DEST_PATH}
+fi
+
+# Mount rootfs  partition
+${MOUNT} ${DEVICE} ${DEST_PATH}/
+
+# Reset FSTAB
+unset FSTAB
 
 while read fstab; do
   case ${fstab} in
@@ -51,5 +78,33 @@ while read fstab; do
       ;;
 
   esac
-${ECHO} -e "device <${device}>\n";
-done < ${FSTAB_FILE}
+
+  # Fill filesystem table FSTAB
+  if [[ ${mount} == / ]]; then
+    FSTAB=("${device} ${mount} ${type} ${options} ${dump} ${pass}" "${FSTAB[@]}")
+    continue
+  elif [[ ${device} =~ ^/* ]]; then 
+    FSTAB=("${FSTAB[@]}" "${device} ${mount} ${type} ${options} ${dump} ${pass}")
+  fi
+
+  # Mount all partitions
+  ${MKDIR} -p ${DEST_PATH}${mount}
+  ${MOUNT} ${device} ${DEST_PATH}${mount}
+done < ${DEST_PATH}/etc/fstab
+
+# Binding the virtual filesystems
+${MOUNT} --bind /dev ${DEST_PATH}/dev
+${MOUNT} -t proc none ${DEST_PATH}/proc
+${MOUNT} -t sysfs none ${DEST_PATH}/sys
+
+# Unbinding the virtual filesystems
+${UMOUNT} ${DEST_PATH}/{dev,proc,sys}
+
+# Umount all partitions
+for (( index=${#FSTAB[@]}-1 ; index>=0 ; index-- )) ; do
+  IFS=$' \t' read device mount type options dump pass <<< "${FSTAB[index]}"
+
+  ${UMOUNT} ${DEST_PATH}${mount}
+done
+
+exit 0
